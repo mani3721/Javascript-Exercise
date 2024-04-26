52eb5a603704f90ec999f91958a220a894eccb85
Gopisakthivel <gopisakthivel@alphabsolutions.com>
2024-04-26 12:15:57 +0530

    added
    Signed-off-by: Gopisakthivel <gopisakthivel@alphabsolutions.com>

--- drag-drop/script.js
+++ drag-drop/script.js
@@ -1,20 +1,20 @@
-function onStartDrag(ev) {
+ function onStartDrag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
 }
 
-function onDrop(ev) {
-    ev.preventDefault();
-    const id = ev.dataTransfer.getData("text");
+ function onDrop(ev) {
+        ev.preventDefault();
+        const id = ev.dataTransfer.getData("text");
 
-    if (id.startsWith("source") && ev.target.id == "destination" && !document.getElementById("newId_" + id)) {
-        const clone = document.getElementById(id).cloneNode(true);
-        clone.id = "newId_" + id;
-        ev.target.appendChild(clone);
-        document.getElementById(id).classList.add("dragged"); 
-        document.getElementById(id).style.display = "none"; 
+        if (id.startsWith("source") && ev.target.id == "destination" && !document.getElementById("newId_" + id)) {
+            const clone = document.getElementById(id).cloneNode(true);
+            clone.id = "newId_" + id;
+            ev.target.appendChild(clone);
+            document.getElementById(id).classList.add("dragged"); 
+            document.getElementById(id).style.display = "none"; 
+        }
+        ev.currentTarget.style.background = "";
     }
-    ev.currentTarget.style.background = "";
-}
 
 function onDragOver(ev) {
     ev.preventDefault();
@@ -31,3 +31,8 @@ function onDragLeave(ev) {
 function onEndDrag(ev) {
     ev.target.style.border = "3px dashed #ff00ff";
 }
+
+document.querySelector('.parent').addEventListener('click', function(e){
+    console.log(e.currentTarget.id, "currentTarget");
+    console.log(e.target.id, "Target");
+})
